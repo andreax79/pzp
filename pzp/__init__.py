14060c7ca0b812e623fb4691e94d51a9c25ae44f
Andrea Bonomi <andrea.bonomi@gmail.com>
2022-11-16 21:59:34 +0000

    v0.0.17

--- pzp/__init__.py
+++ pzp/__init__.py
@@ -15,7 +15,7 @@ from .info import InfoStyle
 from .exceptions import AcceptAction, AbortAction, CustomAction, GenericAction
 from typing import Any, Callable, Iterator, Optional, Sequence, Set, Type, Union
 
-__version__ = "0.0.16"
+__version__ = "0.0.17"
 "PZP Version"
 
 __all__ = [
